899f3b28fd2ba351a970f710aa82a6d5b6124a03
Corey Sewell <corey@gdmedia.tv>
2015-03-26 13:50:20 +1300

    Allow files linked to VideoEmbed to be inseted by html editor

--- code/fields/VideoHtmlEditorField.php
+++ code/fields/VideoHtmlEditorField.php
@@ -18,20 +18,33 @@ class HtmlEditorField_Toolbar_Extension extends HtmlEditorField_Toolbar {
         $result  = false;
         if ($origUrl = $request->getVar('FileURL')) {
             if (Director::is_site_url($origUrl) && VideoEmbed::GetByURL($origUrl)) {
-                $video       = VideoEmbed::GetByURL($origUrl);
-                $fileWrapper = new HtmlEditorField_Embed($origUrl, $video);
-                $refObject   = new ReflectionObject($fileWrapper);
-                $refProperty = $refObject->getProperty('oembed');
-                $refProperty->setAccessible(true);
-                $refProperty->setValue($fileWrapper, $video->GetOembed());
-                $fields      = $this->getFieldsForFile($origUrl, $fileWrapper);
-                $this->extend('updateFieldsForFile', $fields, $origUrl, $fileWrapper);
-                $result      = $fileWrapper->customise(array(
-                            'Fields' => $fields,
-                        ))->renderWith($this->templateViewFile);
+                $video  = VideoEmbed::GetByURL($origUrl);
+                $result = $this->GetResultForVideo($video);
             }
+        } else if ($fileId = $request->getVar('ID')) {
+            $video  = VideoEmbed::get()->filter(array("HTML5VideoID" => $fileId))->first();
+            $result = $this->GetResultForVideo($video);
         }
+
         return $result ? $result : parent::viewfile($request);
+    }
+
+    protected function GetResultForVideo($video) {
+        $result = false;
+        if ($video && $video->exists() && $video->HTML5Video()->exists()) {
+            $fileURL     = $video->HTML5Video()->GetURL();
+            $fileWrapper = new HtmlEditorField_Embed($fileURL, $video);
+            $refObject   = new ReflectionObject($fileWrapper);
+            $refProperty = $refObject->getProperty('oembed');
+            $refProperty->setAccessible(true);
+            $refProperty->setValue($fileWrapper, $video->GetOembed());
+            $fields      = $this->getFieldsForFile($fileURL, $fileWrapper);
+            $this->extend('updateFieldsForFile', $fields, $fileURL, $fileWrapper);
+            $result      = $fileWrapper->customise(array(
+                        'Fields' => $fields,
+                    ))->renderWith($this->templateViewFile);
+        }
+        return $result;
     }
 
 }
